7441dac2675d06c65b4ff624aa753fd3d19b4d78
elin <suyeon101010356@gmail.com>
2024-08-17 09:58:26 -0400

    slope -> Slope in main.py removed hello world.py, and practice.py src

--- src/calculator/main.py
+++ src/calculator/main.py
@@ -3,7 +3,7 @@ import calc_functions
 while True:
 
     print("Hello, welcome to the calculator. What would you like to do?")
-    options = ["Addition", "Subtraction", "Multiplication", "Division", "Average", "slope"]
+    options = ["Addition", "Subtraction", "Multiplication", "Division", "Average", "Slope"]
     print(options)
     action = input("What do you want to do?").lower()
 
